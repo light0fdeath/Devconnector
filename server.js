c630b1d9d094347761339227b67089b989fcb21b
light0fdeath <bigyan.khanal7@gmail.com>
2021-12-08 01:57:04 +1100

    Second Commit

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const connectDB = require('./config/db');
 
 const app = express();
@@ -8,7 +7,11 @@ const app = express();
 
 connectDB();
 
-app.get('/', (req, res) => res.send('API runing'));
+//Init middleware
+app.use(express.json({ extended: false }));
+
+
+app.get('/', (req, res) => res.send('API running'));
 
 // Define Routes
 
